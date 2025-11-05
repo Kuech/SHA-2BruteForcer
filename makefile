1912f18ce8ff4333de9cb7c7b452c5d9d273bbcc
dkuch <dkuch@protonmail.com>
2025-11-05 22:41:01 +0100

    Added first sha2 lib prototype

--- makefile
+++ makefile
@@ -8,5 +8,9 @@ src/brute.o: src/brute.cpp
 
 src/sha2/sha2.o:
 	cd src/sha2 && make
+
+sha2libTest: src/sha2lib/sha2.cpp
+	$(COMPILER) -std=c++17 -Wall -Wextra $^ -o $@
+
 clean:
 	rm -f brute src/brute.o src/sha2/sha2.o
---
+++ src/sha2lib/sha2.cpp
@@ -0,0 +1,60 @@
+#include <bitset>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <memory>
+
+#define ROTATE(r, i) ((i >> r) | (i << (32 - r)))
+// Check out https://sha256algorithm.com/
+using namespace std;
+
+uint8_t* build_msg_block(char* input);
+
+int main(int argc, char **argv) {
+
+    uint8_t* msg = build_msg_block(argv[1]);
+
+    for(int i=0;i<16;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cout << bitset<8>(msg[(i*4)+j]) << ' ';
+        }
+        cout << '\n';
+    }
+    destroy(msg, msg+64);
+}
+
+uint8_t* build_msg_block(char* input)
+{
+    int length = strlen(input);
+    if(strlen(input) > 56)
+    {
+        cout << "Message too long" << '\n';
+        return nullptr_t();
+    }
+
+    uint8_t* msg = new uint8_t[64]; // 512 bit
+    char* end = &input[length];
+    uint count = 0;
+    while(input<end)
+    {
+        msg[count] = *input;
+        count++;
+        input++;
+    }
+
+    msg[count] = 0b10000000;
+    count++;
+
+    for(uint i=count;i<62;i++)
+    {
+        msg[i] = 0;
+    }
+
+    uint16_t lenghInBit = length * 8;
+    msg[62] = (lenghInBit >> 8) & 0xFF;
+    msg[63] =  lenghInBit & 0xFF;
+    return msg;
+}
