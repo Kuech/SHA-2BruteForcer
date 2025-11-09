9599c8b180505b87e8ab5e3c7e87cb8491e5078f
dkuch <dkuch@protonmail.com>
2025-11-09 17:40:29 +0100

    Improved message block initailzation

--- src/sha2lib/sha2.cpp
+++ src/sha2lib/sha2.cpp
@@ -1,3 +1,4 @@
+#include "sha2.h"
 #include <bitset>
 #include <cstddef>
 #include <cstdint>
@@ -5,30 +6,19 @@
 #include <iostream>
 #include <memory>
 
-#define ROTATE(r, i) ((i >> r) | (i << (32 - r)))
 // Check out https://sha256algorithm.com/
 using namespace std;
 
 uint8_t* build_msg_block(char* input);
 
-int main(int argc, char **argv) {
-
-    uint8_t* msg = build_msg_block(argv[1]);
-
-    for(int i=0;i<16;i++)
-    {
-        for(int j=0;j<4;j++)
-        {
-            cout << bitset<8>(msg[(i*4)+j]) << ' ';
-        }
-        cout << '\n';
-    }
-    destroy(msg, msg+64);
+uint8_t* hash_sha256(uint8_t* input)
+{
+    return nullptr_t();
 }
 
 uint8_t* build_msg_block(char* input)
 {
-    int length = strlen(input);
+    uint length = strlen(input);
     if(strlen(input) > 56)
     {
         cout << "Message too long" << '\n';
@@ -48,13 +38,30 @@ uint8_t* build_msg_block(char* input)
     msg[count] = 0b10000000;
     count++;
 
-    for(uint i=count;i<62;i++)
+    for(uint i=count;i<56;i++)
     {
         msg[i] = 0;
     }
 
-    uint16_t lenghInBit = length * 8;
-    msg[62] = (lenghInBit >> 8) & 0xFF;
-    msg[63] =  lenghInBit & 0xFF;
+    uint64_t lenghInBit =length * 8;
+    for(size_t i=0;i<8;i++)
+    {
+        msg[56+i] = lenghInBit >> (56 - 8 * i) & 0xFF;
+    }
+
     return msg;
+}
+int main(int argc, char **argv) {
+
+    uint8_t* msg = build_msg_block(argv[1]);
+
+    for(int i=0;i<16;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cout << bitset<8>(msg[(i*4)+j]) << ' ';
+        }
+        cout << '\n';
+    }
+    destroy(msg, msg+64);
 }
