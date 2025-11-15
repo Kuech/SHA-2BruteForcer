780e703e989acfb4c0e61ad1a6896b4365035a0a
dkuch <dkuch@protonmail.com>
2025-11-15 01:50:44 +0100

    Implemented Sha256 class

--- src/sha2lib/sha2.cpp
+++ src/sha2lib/sha2.cpp
@@ -1,24 +1,12 @@
 #include "sha2.h"
-#include <bitset>
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
 #include <iostream>
-#include <memory>
 
+#define ROTATE(i, r) ((i >> r) | (i << (32 - r)))
 // Check out https://sha256algorithm.com/
 using namespace std;
-
-const static uint32_t h[8] = {
-    0x6a09e667,
-    0xbb67ae85,
-    0x3c6ef372,
-    0xa54ff53a,
-    0x510e527f,
-    0x9b05688c,
-    0x1f83d9ab,
-    0x5be0cd19
-};
 
 const static uint32_t k[64] = {
    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -31,13 +19,42 @@ const static uint32_t k[64] = {
    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 
 };
 
-struct message_block
+Sha256::Sha256(const std::string message)
 {
-    uint length;
-    uint8_t* block;
-};
+    build_msg_block(message, &this->msg_block);
+    uint round = this->msg_block.length/64;
+    for(uint i=0;i<round;i++)
+    {
+        uint32_t pre_proc_msg[64];
+        memset(pre_proc_msg, 0, 64);
+        pre_process_step(&this->msg_block.block[i*64], &pre_proc_msg[0]);
+        hash_sha256(pre_proc_msg, &this->sha256_32bit_entry[0]);
+    }
+}
 
-void hash_sha256(const uint32_t* input, uint32_t sha256_32bit_entry[8])
+Sha256::Sha256(const uint32_t _sha256_32bit_entry[8])
+{
+    copy(_sha256_32bit_entry,_sha256_32bit_entry+8,this->sha256_32bit_entry);
+}
+
+Sha256::~Sha256()
+{
+    delete[] this->msg_block.block;
+}
+
+bool Sha256::operator==(const Sha256 hashedMessage)
+{
+    for(int i=0;i<8;i++)
+    {
+        if(this->sha256_32bit_entry[i] != hashedMessage.sha256_32bit_entry[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Sha256::hash_sha256(const uint32_t* input, uint32_t sha256_32bit_entry[8])
 {
     uint32_t la = sha256_32bit_entry[0];
     uint32_t lb = sha256_32bit_entry[1];
@@ -77,9 +94,8 @@ void hash_sha256(const uint32_t* input, uint32_t sha256_32bit_entry[8])
     sha256_32bit_entry[7] += lh;
 }
 
-void pre_process_step(const uint8_t* chunk, uint32_t chunk_32bit_entry[64])
+void Sha256::pre_process_step(const uint8_t* chunk, uint32_t chunk_32bit_entry[64])
 {
-
     for(int i=0;i<64;i++)
     {
         // Yeah I know it's ugly, but if you know bitwise operations, it makes sense...
@@ -94,25 +110,20 @@ void pre_process_step(const uint8_t* chunk, uint32_t chunk_32bit_entry[64])
     }
 }
 
-void build_msg_block(const string input, message_block* msg)
+void Sha256::build_msg_block(const string input, message_block* msg)
 {
-    cout << "length = " << input.length() << '\n';
-    cout << input.data() << '\n';
-
-    const uint r=64-((input.length()+1+8)%64);
-    msg->length = (input.length()+1+8)+r;;
+    const uint remaining_bits=64-((input.length()+1+8)%64);
+    msg->length = (input.length()+1+8)+remaining_bits;;
     msg->block = new uint8_t[msg->length];
-    cout << "r = " << r << '\n';
-    cout << "block size = " << msg->length << '\n';
-
     memset(msg->block, 0, msg->length);
+
     memcpy(msg->block,input.data(),input.length());
     msg->block[input.length()] = 0b10000000;
 
-    uint64_t lenghInBit =input.length() * 8;
+    uint64_t stringlengthInBit =input.length() * 8;
     for(size_t i=0;i<8;i++)
     {
-        msg->block[(msg->length-8)+i] = lenghInBit >> (56 - 8 * i) & 0xFF;
+        msg->block[(msg->length-8)+i] = stringlengthInBit >> (56 - 8 * i) & 0xFF;
     }
 }
 
@@ -127,42 +138,11 @@ void printSha256(const uint32_t* sha256Hash)
 
 void testSha256Hash(const string inputString, const uint32_t* expectedSha2Hash)
 {
-    message_block msg_block;
-    uint32_t sha256_32bit_entry[8] = {
-        h[0],
-        h[1],
-        h[2],
-        h[3],
-        h[4],
-        h[5],
-        h[6],
-        h[7],
-    };
-    build_msg_block(inputString, &msg_block);
-    uint round = msg_block.length/64;
-    
-    cout << "msg block length : " << msg_block.length << '\n';
-    cout << "Round : " << round << '\n';
-    for(uint i=0;i<round;i++)
-    {
-        uint32_t pre_proc_msg[64];
-        memset(pre_proc_msg, 0, 64);
-        pre_process_step(&msg_block.block[i*64], &pre_proc_msg[0]);
-        hash_sha256(pre_proc_msg, &sha256_32bit_entry[0]);
-    }
-    bool isTestSuccess = true;
-
-    // SHA-256
-    for(int i=0;i<8;i++)
-    {
-        if(sha256_32bit_entry[i] != expectedSha2Hash[i])
-        {
-            isTestSuccess = false;
-        }
-    }
+    Sha256 hashedString = Sha256(inputString);
+    Sha256 expectedSha256 = Sha256(expectedSha2Hash);
 
     string testResult;
-    if(isTestSuccess)
+    if(hashedString == expectedSha256)
     {
         testResult = "✅";
     }
@@ -176,9 +156,8 @@ void testSha256Hash(const string inputString, const uint32_t* expectedSha2Hash)
     cout << "Expected hash =>\n";
     printSha256(expectedSha2Hash);
     cout << "Actual hash =>\n";
-    printSha256(sha256_32bit_entry);
+    printSha256(hashedString.sha256_32bit_entry);
     cout << "================================================================================================\n\n";
-    delete[] msg_block.block;
 }
 
 int main() {
--- src/sha2lib/sha2.h
+++ src/sha2lib/sha2.h
@@ -1,5 +1,34 @@
+#include <cstddef>
 #include <cstdint>
+#include <cstring>
+#include <iostream>
 
-#define ROTATE(i, r) ((i >> r) | (i << (32 - r)))
+struct message_block
+{
+    uint length;
+    uint8_t* block;
+};
 
-uint8_t* hash_sha256(uint32_t* input);
+class Sha256
+{
+private:
+    void build_msg_block(const std::string input, message_block* msg);
+    void pre_process_step(const uint8_t* chunk, uint32_t chunk_32bit_entry[64]);
+    void hash_sha256(const uint32_t* input, uint32_t sha256_32bit_entry[8]);
+    message_block msg_block;
+public:
+    uint32_t sha256_32bit_entry[8] = {
+    0x6a09e667,
+    0xbb67ae85,
+    0x3c6ef372,
+    0xa54ff53a,
+    0x510e527f,
+    0x9b05688c,
+    0x1f83d9ab,
+    0x5be0cd19
+    };
+    Sha256(const std::string message);
+    Sha256(const uint32_t _sha256_32bit_entry[8]);
+    ~Sha256();
+    bool operator==(const Sha256 hashedMessage);
+};
